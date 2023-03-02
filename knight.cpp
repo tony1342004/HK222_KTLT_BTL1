d3c72330b1774bf0aa39858de11f591e0ef8f463
tony1342004 <89771701+tony1342004@users.noreply.github.com>
2023-03-02 20:45:34 +0700

    Update knight.cpp

--- knight.cpp
+++ knight.cpp
@@ -253,7 +253,7 @@ void asclepius(string &asclepius_pack, int &remedy, int &maidenkiss, int &phoeni
                 i++;
             }
             k = "";
-            if (i == r1 - 1)
+            if (i == r1 - 1 && m == c1 - 1)
                 break;
         }
         t++;
